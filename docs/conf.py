4f6f90233787bf30174febe618d683dbf5b0ddab
Iain-S <25081046+Iain-S@users.noreply.github.com>
2023-09-19 10:39:16 +0100

    Add theme and logo

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,7 @@ from importlib import metadata
 from unittest.mock import MagicMock
 
 import pulumi
+import sphinx_rtd_theme
 
 import rctab_infrastructure as ri
 
@@ -55,10 +56,11 @@ release = version
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-# -- Options for HTML output -------------------------------------------------
-
-html_theme = "alabaster"
-html_static_path = ["_static"]
+html_logo = "RCTab-hex.png"
+html_theme_options = {
+    "logo_only": True,
+    "display_version": True,
+}
 
 # -- General configuration
 
@@ -72,3 +74,12 @@ extensions = [
 # -- Options for HTML output
 
 html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_static_path = ["_static"]
+
+html_logo = "RCTab-hex.png"
+
+
+def setup(app):
+    """Tasks to perform during app setup."""
+    app.add_css_file("css/custom.css")
