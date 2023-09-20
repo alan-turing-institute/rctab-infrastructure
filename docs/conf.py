9c2638da0bd94598d167d66a49088cde43d02b7b
Iain-S <25081046+Iain-S@users.noreply.github.com>
2023-09-20 12:11:26 +0100

    Autogenerate docs for whole package

--- docs/conf.py
+++ docs/conf.py
@@ -10,33 +10,35 @@ import sphinx_rtd_theme
 
 import rctab_infrastructure as ri
 
-# Add repo root to path for autodoc
-
-sys.path.insert(0, os.path.abspath(".."))
-
 # Patch Pulumi so that importing constants.py doesn't cause errors
 
 
 def require_or_get(*args, **kwargs):
     """A dummy method for pulumi.Config."""
-    if args:
-        if args[0] == "ticker":
-            # A valid ticker
-            return "ab"
-        elif args[0] in ("auto_deploy", "ignore_whitelist"):
-            return "true"
-        elif args[0] == "whitelist":
-            return "00000000-0000-0000-0000-000000000000"
-        elif args[0] == "log_level":
-            return "WARNING"
-        elif args[0] == "db_root_cert_path":
-            return str(pathlib.Path(__file__).absolute())
+    valid_values = {
+        "ticker": "ab",
+        "auto_deploy": "true",
+        "ignore_whitelist": "true",
+        "log_level": "WARNING",
+        "db_root_cert_path": str(pathlib.Path(__file__).absolute()),
+        "whitelist": None,
+    }
+    if args and args[0] in valid_values:
+        return valid_values[args[0]]
     return "something"
 
 
 def secret(*args, **kwargs):
     """A dummy method for pulumi.Config."""
-    return pulumi.Output.secret("something secret")
+    valid_values = {
+        "usage_mgmt_group": None,
+    }
+    if args and args[0] in valid_values:
+        value = valid_values[args[0]]
+    else:
+        value = "something secret"
+
+    return pulumi.Output.secret(value)
 
 
 pulumi.Config = MagicMock()
@@ -44,7 +46,7 @@ pulumi.Config.return_value.require.side_effect = require_or_get
 pulumi.Config.return_value.get.side_effect = require_or_get
 pulumi.Config.return_value.get_secret.side_effect = secret
 
-# General configuration
+# -- General configuration
 
 project = "rctab-infrastructure"
 author = "The Alan Turing Institute's Research Computing Team"
@@ -56,19 +58,13 @@ release = version
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_logo = "RCTab-hex.png"
-html_theme_options = {
-    "logo_only": True,
-    "display_version": True,
-}
-
-# -- General configuration
-
 extensions = [
-    "sphinx.ext.duration",
-    "sphinx.ext.doctest",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.napoleon",
+    "myst_parser",
 ]
 
 # -- Options for HTML output
@@ -77,9 +73,19 @@ html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ["_static"]
 
+html_theme_options = {
+    "logo_only": True,
+    "display_version": True,
+}
+
 html_logo = "RCTab-hex.png"
 
 
 def setup(app):
     """Tasks to perform during app setup."""
     app.add_css_file("css/custom.css")
+
+
+# -- Options for autosummary extension
+
+autosummary_generate = True
--- docs/index.rst
+++ docs/index.rst
@@ -1,14 +1,19 @@
 Welcome to rctab-infrastructure's documentation!
 ================================================
 
-.. automodule:: rctab_infrastructure.api
-    :members:
-
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
+   :glob:
 
    sometext
+
+
+.. autosummary::
+   :toctree: _autosummary
+   :recursive:
+
+   rctab_infrastructure
 
 Indices and tables
 ==================
